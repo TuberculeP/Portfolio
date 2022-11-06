f12b0bef1b26a0c2c32936e8a511be7a16dd3a03
Félix Laviéville <lavieville.felix@gmail.com>
2022-11-06 15:58:28 +0100

    :rocket: deploying final portfolio

--- template/header.php
+++ template/header.php
@@ -1,6 +1,6 @@
 <div id="p5-anim"></div>
 <header>
-    <h2 class="construction">En Cours de Construction</h2>
+    <!-- <h2 class="construction">En Cours de Construction</h2> -->
 	<div id="burger" onclick="this.classList.toggle('active')">
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
